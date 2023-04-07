9883cc37d2dc30a031c4ec18c259c139fa69b1b2
Tructhanh2012 <tructhanh15994@gmail.com>
2023-04-07 14:46:09 +0700

    modify build.xml to run test

--- test/com/truc/mathutil/test/core/MathUtilDDTTest.java
+++ test/com/truc/mathutil/test/core/MathUtilDDTTest.java
@@ -1,3 +1,5 @@
+package com.truc.mathutil.test.core;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -28,7 +30,7 @@ public class MathUtilDDTTest {
          {2, 2},
          {3, 6},
          {4, 24},
-         {5, 120},};
+         {6, 720},};
 
    }
    @Parameterized.Parameter(value = 0) //map với mảng data
