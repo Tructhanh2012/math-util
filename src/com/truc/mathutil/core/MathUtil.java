ff6b6e941c5def8bda6a38a593118db385777820
Tructhanh2012 <tructhanh15994@gmail.com>
2023-04-26 16:47:42 +0700

    refactor the main code and green 4ever always - fix exception msg

--- src/com/truc/mathutil/core/MathUtil.java
+++ src/com/truc/mathutil/core/MathUtil.java
@@ -31,6 +31,9 @@ public class MathUtil {
 //   }
     
    public static long getFactorial(int n) {
+      if(n<0||n>20) {
+         throw new IllegalArgumentException("Invalid argument, n must be between 0 and 20");
+      }
       if(n==0 || n==1) {
          return 1;
       }
